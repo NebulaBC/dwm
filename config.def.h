c89279be675395ac1181375d4bdcf0b04a2e3c3d
NebulaBC <contact@nebulabc.net>
2022-05-19 10:05:29 -0400

    Laptop Scheme (Desktop still on GruvBox)

--- config.def.h
+++ config.def.h
@@ -17,13 +17,13 @@ static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 static const char *fonts[]          = { "Comic Mono:size=12", "3270Medium Nerd Font:size=12" };
 static const char dmenufont[]       = "Comic Mono:size=12";
-static const char col_gray1[]       = "#282a36";
-static const char col_gray2[]       = "#ffb86c";
-static const char col_gray3[]       = "#ff79c6";
-static const char col_gray4[]       = "#ffb86c";
-static const char col_cyan[]        = "#282a36";
-static const unsigned int baralpha = 220;
-static const unsigned int borderalpha = 220;
+static const char col_gray1[]       = "#112c27";
+static const char col_gray2[]       = "#942911";
+static const char col_gray3[]       = "#9D8420";
+static const char col_gray4[]       = "#82A6B1";
+static const char col_cyan[]        = "#112c27";
+static const unsigned int baralpha = 500;
+static const unsigned int borderalpha = 500;
 static const char *colors[][3]      = {
     /*               fg         bg         border   */
     [SchemeNorm] = { col_gray3, col_gray1, col_cyan },
@@ -92,18 +92,20 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *web[]  = { "firefox" };
+static const char *web[]  = { "firefox", NULL };
+static const char *screenshot[] = { "screenshot", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      spawn,          {.v = web } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = screenshot} },
 	/*{ MODKEY,                       XK_b,      togglebar,      {0} },*/
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	/* { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } }, */
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,                       XK_Return, zoom,           {0} },
