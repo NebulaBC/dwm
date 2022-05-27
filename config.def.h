82f34ee990dc54cad9d8b55f2470c7fd7356d9eb
NebulaBC <contact@nebulabc.net>
2022-05-27 11:28:43 -0400

    fix dots

--- config.def.h
+++ config.def.h
@@ -37,7 +37,7 @@ static const unsigned int alphas[][3]      = {
 
 
 /* tagging */
-static const char *tags[] = { "", "", "ﭮ", "", "﬏", "", "", "", "" };
+static const char *tags[] = { "", "", "ﭮ", "", "﬏", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -92,7 +92,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *web[]  = { "firefox", NULL };
+static const char *web[]  = { "chromium", NULL };
 static const char *screenshot[] = { "screenshot", NULL };
 
 static Key keys[] = {
